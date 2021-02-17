aff0cc61a01414d356ffcce0634a286729d8e09f
gregoryturchino <gturchino@gmail.com>
2021-02-17 11:34:42 +0100

    component:navbar + update package

--- src/app/aliments/aliments.module.ts
+++ src/app/aliments/aliments.module.ts
@@ -4,16 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AlimentsRoutingModule } from './aliments-routing.module';
 import { AlimentsComponent } from './aliments.component';
 import { AlimentListComponent } from './aliment-list/aliment-list.component';
-import { SharedModule } from '../shared/shared/shared.module';
-
+import { SharedModule } from '../shared/shared.module';
+import { NavbarComponent } from '../shared/components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AlimentsComponent, AlimentListComponent],
+  declarations: [AlimentsComponent, AlimentListComponent,NavbarComponent],
   imports: [
     CommonModule,
     SharedModule,
     AlimentsRoutingModule,
-
   ]
 })
 export class AlimentsModule { }
